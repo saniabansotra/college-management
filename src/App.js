b1944a2235c89d7c6b812120cb32770729de35a6
Sania Bansotra <saniabansotra@gmail.com>
2024-02-19 19:40:19 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./components/home";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Profile from "./components/profile";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ToastContainer />
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/login" Component={Login} />
+          <Route path="/signup" Component={Signup} />
+          <Route path="/profile" Component={Profile} />
+          {/* <Route path="*" Component={NotFound} /> */}
+        </Routes>
+      </Router>
+    </>
   );
 }
 
