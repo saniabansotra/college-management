fe8385eb0967ea4aa10f29434eb4b7490df7720e
Sania Bansotra <saniabansotra@gmail.com>
2024-02-21 23:08:57 +0530

    message

---
+++ src/components/contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./navbar";
+const Contact = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <h1>Hello this is Contact page</h1>
+    </>
+  );
+};
+export default Contact;
